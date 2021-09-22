d76dc44c1d1b49490cd73190b1b7b73a5113f7aa
hastiafra <77177165+hastiafra@users.noreply.github.com>
2021-09-21 22:36:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+function getWords(){
+    let text = document.getElementById("commentBox").value;
+    document.getElementById("display").innerHTML= " You " + " said " + text + " ! ";
+    
+}
+
